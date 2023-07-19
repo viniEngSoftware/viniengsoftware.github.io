750eeddafaacb70fd3da1956f2ac0563fcb52a81
Vinicius Costa <111258564+viniEngSoftware@users.noreply.github.com>
2023-07-19 00:59:12 -0300

    correção javascript

--- assets/js/menu-mobile.js
+++ assets/js/menu-mobile.js
@@ -4,33 +4,34 @@ const navegacao = document.getElementById("menu-navegacao");
 const navega = document.getElementById("navega");
 const logo = document.querySelector(".logo-img");
 
-function menuE(){
-    navegacao.style.display = "flex";
-    navega.style.height= "100%"
-    logo.style.margin= "-71.8% 0 0 2.5%";
-    menuSair.style.margin = "-131.9% 0 0 88.5%";
+function menuE() {
 
-    if (menu.style.visibility === "visible") {
-        menu.style.visibility = "hidden";
+    menu.style.visibility = "hidden";
+
+    if (menu.style.visibility === "hidden") {
         menuSair.style.visibility = "visible";
-    } else{
-        menu.style.visibility = "visible";
+    } else {
+        menu.style.visibility = "hidden";
     }
+
+    navegacao.style.display = "flex";
+    navega.style.height = "100%";
+    logo.style.margin = "-71.8% 0 0 2.5%";
+    menuSair.style.margin = "-131.9% 0 0 88.5%";
 }
 
+function sairMenu() {
 
-function sairMenu() { 
-    navega.style.height= "8%"
-    logo.style.margin= "-6% 0 0 2.5%";
+    menuSair.style.visibility = "hidden";
 
-    if (menuSair.style.visibility === "visible") {
-        menuSair.style.visibility = "hidden";
+    if (menuSair.style.visibility === "hidden") {
         menu.style.visibility = "visible";
         navegacao.style.display = "none";
-    
-    } else{
-        menuSair.style.visibility = "visible";
+    } else {
+        menuSair.style.visibility = "hidden";
     }
 
+    navega.style.height = "8%";
+    logo.style.margin = "-6% 0 0 2.5%";
 }
 
